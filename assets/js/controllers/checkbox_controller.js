8cade482c79286288094488378ea65c490574ccc
Mitchell Hanberg <mitch@mitchellhanberg.com>
2019-02-10 16:51:42 -0500

    Only allow the owner of an obligation to mark it as complete

--- assets/js/controllers/checkbox_controller.js
+++ assets/js/controllers/checkbox_controller.js
@@ -5,39 +5,47 @@ export default class extends Controller {
 
   connect() {
     this.obligationId = this.data.get("id");
+    this.isOwner = this.data.get("isOwner");
     this.checked = false;
     this.fadeTimeoutId = null;
 
-    this.channel = window.socket.channel(`obligation:${this.obligationId}`, {});
-    this.channel
-      .join()
-      .receive("ok", resp => {
-        console.log("Joined successfully", resp);
-      })
-      .receive("error", resp => {
-        console.log("Unable to join", resp);
-      });
+    if (this.isOwner === "true") {
+      this.channel = window.socket.channel(
+        `obligation:${this.obligationId}`,
+        {}
+      );
+      this.channel
+        .join()
+        .receive("ok", resp => {
+          console.log("Joined successfully", resp);
+        })
+        .receive("error", resp => {
+          console.log("Unable to join", resp);
+        });
 
-    this.channel.on("check_event", ({ image }) => {
-      this.checkboxTarget.innerHTML = image;
-    });
+      this.channel.on("check_event", ({ image }) => {
+        this.checkboxTarget.innerHTML = image;
+      });
+    }
   }
 
   toggle(event) {
-    this.checked = !this.checked;
-    this.channel.push(`check:obligation:${this.obligationId}`, {
-      checked: this.checked
-    });
+    if (this.isOwner === "true") {
+      this.checked = !this.checked;
+      this.channel.push(`check:obligation:${this.obligationId}`, {
+        checked: this.checked
+      });
 
-    if (this.checked === true) {
-      this.element.classList.add("faded");
-      this.fadeTimeoutId = setTimeout(() => {
-        this.element.parentNode.removeChild(this.element);
-      }, 2000);
-    } else {
-      clearTimeout(this.fadeTimeoutId);
-      this.fadeTimeoutId = null;
-      this.element.classList.remove("faded");
+      if (this.checked === true) {
+        this.element.classList.add("faded");
+        this.fadeTimeoutId = setTimeout(() => {
+          this.element.parentNode.removeChild(this.element);
+        }, 2000);
+      } else {
+        clearTimeout(this.fadeTimeoutId);
+        this.fadeTimeoutId = null;
+        this.element.classList.remove("faded");
+      }
     }
   }
 }
