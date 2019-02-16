bd470c9c4aca4008d04b2daa9b26fa3a940ba1a6
Mitchell Hanberg <mitch@mitchellhanberg.com>
2019-02-15 19:07:16 -0500

    Show empty state once all obligations are finished

--- assets/js/controllers/obligation_list_controller.js
+++ assets/js/controllers/obligation_list_controller.js
@@ -6,6 +6,18 @@ export default class extends Controller {
 
   connect() {
     window.addEventListener("click", this.onOutsideClick);
+    const emptyState = document.createElement("div");
+    emptyState.classList.add("text-grey-400", "pl-4");
+    emptyState.appendChild(document.createTextNode("No current obligations"));
+
+    this.observer = new MutationObserver(() => {
+      if (this.element.children.length === 0) {
+        this.element.parentNode.appendChild(emptyState);
+        this.element.parentNode.removeChild(this.element);
+      }
+    });
+
+    this.observer.observe(this.element, { childList: true });
   }
 
   unselectAll() {
@@ -26,5 +38,10 @@ export default class extends Controller {
     event.path
       .find(e => e.nodeName === "LI")
       .classList.add("obligation-selected");
+  }
+
+  disconnect() {
+    window.removeEventListener("click", this.onOutsideClick);
+    this.observer.disconnect();
   }
 }
