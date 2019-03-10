0e3f1b54f7166b5c228d65f764819ad9e2d30620
Mitchell Hanberg <mitch@mitchellhanberg.com>
2019-03-10 19:39:11 -0400

    Only send invite email a user has that email

--- lib/follow_through_web/channels/user_socket.ex
+++ lib/follow_through_web/channels/user_socket.ex
@@ -8,11 +8,12 @@ defmodule FollowThroughWeb.UserSocket do
   def connect(%{"token" => token}, socket, _connect_info) do
     with {:ok, verified_user_id} <-
            Phoenix.Token.verify(FollowThroughWeb.Endpoint, "user salt", token, max_age: 86400),
-         %User{} =  user <- User.get(verified_user_id) do
+         %User{} = user <- User.get(verified_user_id) do
       {:ok, assign(socket, :current_user, user)}
     else
       {:error, _} ->
         :error
+
       nil ->
         :error
     end
--- lib/follow_through_web/controllers/invitation_controller.ex
+++ lib/follow_through_web/controllers/invitation_controller.ex
@@ -1,21 +1,68 @@
 defmodule FollowThroughWeb.InvitationController do
   use FollowThroughWeb, :controller
-  alias FollowThrough.{Invitation, Mailer, Email}
+  alias FollowThrough.Invitation
+
+  defmodule Emailer do
+    require Logger
+    alias FollowThrough.Mailer
+    alias FollowThrough.Email
+    alias FollowThrough.User
+    alias FollowThrough.Invitation
+    alias FollowThrough.Repo
+    defstruct [:send?, :invitation, :params, :error?]
+
+    def new(params) do
+      %__MODULE__{
+        params: params
+      }
+    end
+
+    def create_invite(%__MODULE__{params: params} = emailer) do
+      case Repo.get_by(User, email: params["invited_email"]) do
+        %User{} ->
+          case Invitation.create(params) do
+            {:ok, invite} ->
+              invite = invite |> Invitation.with_team()
+
+              struct(emailer, send?: true, invitation: invite)
+
+            {:error, changeset} ->
+              struct(emailer, send?: false, error?: true)
+          end
+
+        nil ->
+          struct(emailer, send?: false)
+      end
+    end
+
+    def send(%__MODULE__{invitation: invitation, send?: send?} = emailer) do
+      if send? do
+        Logger.debug(fn -> "Sending invitation email to #{inspect(invitation.invited_email)}" end)
+
+        invitation
+        |> Email.invitation_email()
+        |> Mailer.deliver_now()
+      else
+        Logger.debug(fn -> "Not sending invitation email." end)
+      end
+
+      emailer
+    end
+  end
 
   def create(conn, %{"invitation" => params}) do
-    with {:ok, invitation} <-
-           params
-           |> Invitation.create(),
-         invitation
-         |> Invitation.with_team()
-         |> Email.invitation_email()
-         |> Mailer.deliver_now() do
-      conn
-      |> put_flash(:info, "Successfully invited #{params["invited_email"]}")
-    else
-      _ ->
+    params
+    |> Emailer.new()
+    |> Emailer.create_invite()
+    |> Emailer.send()
+    |> case do
+      %Emailer{error?: true} ->
         conn
         |> put_flash(:error, "Something went wrong, please contact support")
+
+      _ ->
+        conn
+        |> put_flash(:info, "Successfully invited #{params["invited_email"]}")
     end
     |> redirect(to: Routes.team_path(conn, :show, params["team_id"]))
   end
