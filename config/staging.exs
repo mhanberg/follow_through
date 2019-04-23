7d29a8c661c24bd646a14d6f843e7c59d601ad44
Mitchell Hanberg <mitch@mitchellhanberg.com>
2019-04-23 17:01:40 -0400

    turn off custom domains

--- config/staging.exs
+++ config/staging.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :follow_through, FollowThroughWeb.Endpoint,
   http: [port: System.get_env("PORT")],
-  url: [scheme: "https", host: "staging.followthrough.app", port: 443],
+  url: [scheme: "https", host: "follow-through-staging.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
